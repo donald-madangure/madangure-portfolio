5bbf63f171ff83e95f15e1d38af94a9227019357
dmadangure <donaldmadangure@gmail.com>
2026-01-23 09:48:50 +0200

    pitch update

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -11,14 +11,17 @@ const Header = () => {
   }, [])
 
   return (
-    <header id="header">      
+    <header id="header" className='mb-6'>      
       <div className="container header__container">
         <div className="header__profile">
           <img src={HeaderImage} alt="Header Portrait" />
         </div>
         <h3  data-aos="fade-up">Donald Madangure</h3>
         <p  data-aos="fade-up">
-          Ready to launch your online presence? <b>Tell me about your project</b> and let's create <b>fast, modern, and fully mobile-responsive website or web application </b> including <b>cutting-edge AI Integration (eg VAPI) </b> to enhance user experience and functionality!          
+          Ready to launch your online presence? Tell me about your project and let's create a fast, modern, and fully mobile-responsive website or web application.
+          I specialize in building <b>Progressive Web Apps (PWAs)</b>—giving your users a lightning-fast, app-like experience directly through their browser,
+          complete with offline capabilities and push notifications. To further elevate your platform, I offer cutting-edge <b>AI Integration</b>
+          to create intuitive, voice-enabled, and highly functional user experiences that set you apart from the competition!         
         </p>
         <div className="header__cta"  data-aos="fade-up">
           <a href="#contact" className='btn primary'>Let's Talk</a>
