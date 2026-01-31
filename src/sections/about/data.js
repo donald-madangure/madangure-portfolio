4abae637f715a26d2c8f174920e61bf71797f2ef
dmadangure <donaldmadangure@gmail.com>
2026-01-31 21:12:31 +0200

    fix: fixing my about sectionj

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -4,7 +4,7 @@ import {BiHappyHeartEyes} from 'react-icons/bi'
 
 
 const data = [
-    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '3+ Years Working'},
+    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '6+ Years Working'},
     {id: 2, icon: <TbBooks/>, title: 'Projects', desc: '30+ Completed'},
     {id: 3, icon: <BiHappyHeartEyes/>, title: 'Clients', desc: '15+ happy clients'}
     ]
